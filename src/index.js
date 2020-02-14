049ab1c66171153f3d56b068bc13dcf3e5875a17
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-14 18:59:41 +0300

    работа с разными типами файлов

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import Post from "./post";
 
+import('./styles.css');
+
+import json from "./assets/json";
+
 const post = new Post('Webpack post title');
 
 console.log('Post to string: ', post.toString());
+
+console.log('JSON: ', json);
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
     ],
     module: {
         rules: [
-
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
         ]
     }
 }
