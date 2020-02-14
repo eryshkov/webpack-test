d7bd15b81a58dc22b7da67badb0b44b01c515d50
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-14 18:36:10 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js',
+        analytics: './analytics.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "Evgenii App",
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+
+        ]
+    }
+}
